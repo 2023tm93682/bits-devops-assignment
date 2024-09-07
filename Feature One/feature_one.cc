8a5ea088da4a3c82b92dd15e8d5e35769689c775
rajathvs <vsr291999@gmail.com>
2024-09-07 21:35:40 +0530

    feat: Add subtraction logic

--- Feature One/feature_one.cc
+++ Feature One/feature_one.cc
@@ -8,17 +8,17 @@ int main() {
   cout << "Enter two integers to perform calculations: ";
   cin >> first_number >> second_number;
 
-  // sum of two numbers is stored in variable sum
+  // Step 1: Use sum for the addition
   sum = second_number + first_number;
 
   // prints sum
   cout << first_number << " + " <<  second_number << " = " << sum << endl;
 
-  // conflict: reuse sum variable to store the product of two numbers
-  sum = second_number * first_number;
+  // Branch 2: Add subtraction functionality
+  sum = second_number - first_number;
 
-  // prints product
-  cout << first_number << " * " << second_number << " = " << sum << endl;
+  // prints difference
+  cout << second_number << " - " << first_number << " = " << sum << endl;
 
   return 0;
 }
